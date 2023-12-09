e597c8116f954a7b4954f466854da9ea95ad7db7
root <+>
2023-12-09 12:09:25 -0700

    experiment

--- client/src/components/DownloadPage.jsx
+++ client/src/components/DownloadPage.jsx
@@ -12,7 +12,7 @@ function DownloadPage( { birthdate, name, breed, gender, parent } ) {
     function handleClick(e) {
         e.preventDefault()
 
-        fetch ('http://127.0.0.1:5555/infopackage', {
+        fetch ('http://0.0.0.0:10000/infopackage', {
             method: 'POST',
             headers: {'content-type':'application/json'},
             body: JSON.stringify( {
